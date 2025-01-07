5706feb4e9225c9d31a69d288980229f403471b3
sayidamiah <ec23151@qmul.ac.uk>
2025-01-07 23:03:36 +0000

    second test to check files exist

--- Tests/test_unit.py
+++ Tests/test_unit.py
@@ -1,13 +1,20 @@
 import unittest
 import pandas as pd
+import os
 import matplotlib as plt
 class TestDataLoading(unittest.TestCase):
 
 
     def setUp(self):
         # Paths to the CSV files
-        self.working_hours_path = 'annual-working-hours-per-worker.csv'
-        self.productivity_path = 'labor-productivity-per-hour-pennworldtable.csv'
+        self.working_hours_path = 'Data/annual-working-hours-per-worker.csv'
+        self.productivity_path = 'Data/labor-productivity-per-hour-pennworldtable.csv'
+    
+    def test_files_exist(self):
+        # Check if the files exist
+        self.assertTrue(os.path.exists(self.working_hours_path), "Working hours file does not exist.")
+        self.assertTrue(os.path.exists(self.productivity_path), "Productivity file does not exist.")
+
 
 # Run the tests
 if __name__ == "__main__":
