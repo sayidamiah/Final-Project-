fc6a97f409b2a2192bfd8fde08def0a3eb18ae9e
sayidamiah <ec23151@qmul.ac.uk>
2025-01-08 20:59:01 +0000

    merged datasets

--- Plots/function.py
+++ Plots/function.py
@@ -2,5 +2,8 @@ import pandas as pd
 import matplotlib as plt 
 
 # Load datasets
-working_hours = pd.read_csv('annual-working-hours-per-worker.csv')
-productivity = pd.read_csv('labor-productivity-per-hour-pennworldtable.csv')
+working_hours_data = pd.read_csv('Data/annual-working-hours-per-worker.csv')
+productivity_data = pd.read_csv('Data/labor-productivity-per-hour-pennworldtable.csv')
+
+# Merge datasets
+merged_data = pd.merge(productivity_data, working_hours_data, on=['Entity', 'Year'])
