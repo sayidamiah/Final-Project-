34862c7c899c1ca9f899b164d76232d2b4604456
sayidamiah <ec23151@qmul.ac.uk>
2025-01-08 21:13:12 +0000

    renamed columns for readability

--- Plots/function.py
+++ Plots/function.py
@@ -1,9 +1,15 @@
 import pandas as pd 
 import matplotlib as plt 
 
-# Load datasets
+#Loading datasets
 working_hours_data = pd.read_csv('Data/annual-working-hours-per-worker.csv')
 productivity_data = pd.read_csv('Data/labor-productivity-per-hour-pennworldtable.csv')
 
-# Merge datasets
+#Merging datasets
 merged_data = pd.merge(productivity_data, working_hours_data, on=['Entity', 'Year'])
+
+#Renaming the datasets
+merged_data.rename(columns={
+    'Productivity: output per hour worked': 'Productivity',
+    'Average annual working hours per worker': 'Working Hours'
+}, inplace=True)
